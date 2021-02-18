abf6dc3b03d42d6bc9e6278a06a1907f61aa44b5
Andrew delange <andrew@ix.co.za>
2021-02-18 11:02:39 +0200

    Set up base app

--- FirstWebSite/App_Start/RouteConfig.cs
+++ FirstWebSite/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace FirstWebSite
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
---
+++ FirstWebSite/Controllers/MediaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebSite.Controllers
+{
+    public class MediaController : Controller
+    {
+        public ActionResult Print()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
